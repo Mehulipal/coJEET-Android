cf947ae462a3b72564b13692c1ffaa3617825668
Mehuli <mehuli_1901cb27@iitp.ac.in>
2020-05-01 17:45:12 +0530

    recyclerview materialcardview done

---
+++ app/src/main/java/com/example/mevscovid/PoliciesClass.java
@@ -0,0 +1,43 @@
+package com.example.mevscovid;
+
+public class PoliciesClass {
+
+
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+
+
+    private String mPolicy_name;
+
+    private String mPolicy_description;
+
+   // private Integer mPolicy_image_id;
+
+    private String mVisit_us;
+
+    public PoliciesClass(String Policy_name, String Policy_description, String Visit_us) {
+        mPolicy_name = Policy_name;
+        mPolicy_description = Policy_description;
+        //mPolicy_image_id=Policy_image_id;
+        mVisit_us=Visit_us;
+    }
+
+
+    public String getmPolicy_name() {
+        return mPolicy_name;
+    }
+    public String getmPolicy_description() {
+        return mPolicy_description;
+    }
+
+    /**public Integer getmPolicy_image_id(){
+        return mPolicy_image_id;
+    }**/
+
+    public String getmVisit_us() {
+        return mVisit_us;
+    }
+}
--- app/src/main/java/com/example/mevscovid/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mevscovid/ui/home/HomeFragment.java
@@ -12,24 +12,70 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.mevscovid.PoliciesClass;
 import com.example.mevscovid.R;
+
+
+
+
+
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
+
+        import androidx.annotation.Nullable;
+        import androidx.annotation.NonNull;
+        import androidx.fragment.app.Fragment;
+        import androidx.lifecycle.Observer;
+        import androidx.lifecycle.ViewModelProviders;
+        import androidx.recyclerview.widget.GridLayoutManager;
+        import androidx.recyclerview.widget.RecyclerView;
+
+        import com.example.mevscovid.R;
+import com.example.mevscovid.adapter;
+
+import java.util.ArrayList;
+        import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    public HomeFragment() {
     }
+
+    private com.example.mevscovid.adapter adapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Log.e(".PoliciesFragment","Worked fine before");
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_policies);
+        Log.e(".PoliciesFragment","Worked now??????????????????????");
+
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+
+        adapter = new adapter(getContext());
+        recyclerView.setAdapter(adapter);
+
+        populateData();
+
+        return view;
+    }
+
+    private void populateData() {
+
+        ArrayList<PoliciesClass> PolicyItems = new ArrayList<>();
+        PolicyItems.add(new PoliciesClass("Policy Name 1","Policy Description 1","Visit US 1"));
+        PolicyItems.add(new PoliciesClass("Policy Name 2","Policy Description 2","Visit US 2"));
+        PolicyItems.add(new PoliciesClass("Policy Name 3","Policy Description 3","Visit US 3"));
+        PolicyItems.add(new PoliciesClass("Policy Name 4","Policy Description 4","Visit US 4"));
+        PolicyItems.add(new PoliciesClass("Policy Name 5","Policy Description 5","Visit US 5"));
+        PolicyItems.add(new PoliciesClass("Policy Name 6","Policy Description 6","Visit US 6"));
+
+        adapter.setPoliciesAdapter(PolicyItems);
+
+    }
+
 }
