0bbc236f84e9d1d7723b1240c44429453ecd18bd
Mehuli <mehuli_1901cb27@iitp.ac.in>
2020-05-30 22:19:52 +0530

    Text recognition

--- app/src/main/java/com/example/mevscovid/ui/share/ShareFragment.java
+++ app/src/main/java/com/example/mevscovid/ui/share/ShareFragment.java
@@ -1,10 +1,17 @@
 package com.example.mevscovid.ui.share;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,23 +20,108 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.mevscovid.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ShareFragment extends Fragment {
 
     private ShareViewModel shareViewModel;
+    private Button captureImageBtn, detectTextBtn;
+    private ImageView imageView;
+    private TextView textView;
+    Bitmap imageBitmap;
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        shareViewModel =
-                ViewModelProviders.of(this).get(ShareViewModel.class);
+        //  shareViewModel =
+        //        ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.fragment_share, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
-        shareViewModel.getText().observe(this, new Observer<String>() {
+
+        /**  shareViewModel.getText().observe(this, new Observer<String>() {
+        @Override
+        public void onChanged(@Nullable String s) {
+        textView.setText(s);
+        }
+        });**/
+        captureImageBtn = root.findViewById(R.id.capture_image);
+        detectTextBtn = root.findViewById(R.id.detect_text_image);
+        imageView = root.findViewById(R.id.image_view);
+        textView = root.findViewById(R.id.text_display);
+
+        captureImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+        detectTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detectTextFromImage();
+                textView.setText("");
             }
         });
         return root;
     }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void detectTextFromImage() {
+
+        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+        firebaseVisionTextRecognizer.processImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText texts) {
+                displayTextFromImage(texts);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void displayTextFromImage(FirebaseVisionText texts) {
+
+        List<FirebaseVisionText.TextBlock> blockList = texts.getTextBlocks();
+        if(blockList.size() == 0)
+        {
+            Toast.makeText(getActivity(),"No Text Found in Image",Toast.LENGTH_SHORT);
+        }
+        else {
+            for (FirebaseVisionText.TextBlock block : texts.getTextBlocks()) {
+                String text = block.getText();
+                textView.setText(text);
+            }
+        }
+    }
 }
+
+
