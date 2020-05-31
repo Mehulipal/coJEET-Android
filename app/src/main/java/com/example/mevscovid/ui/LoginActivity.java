63a725bcbe58cb717c54afa0e27a82ea0ecd2476
Mehuli <mehuli_1901cb27@iitp.ac.in>
2020-05-31 15:20:22 +0530

    splash screen

--- app/src/main/java/com/example/mevscovid/ui/LoginActivity.java
+++ app/src/main/java/com/example/mevscovid/ui/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.mevscovid.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -102,6 +103,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         emailId = findViewById(R.id.email);
